f55c15041ee340c49ebf9e9d5fefda9856be5efb
Guilherme Cezario <cezariogui@outlook.com>
2022-10-12 12:02:56 -0300

    config core.ignorecase false

---
+++ src/assets/Logo.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25px"
+      height="25px"
+      version="1.1"
+      viewBox="0 0 161.967 151.224"
+    >
+      <g transform="translate(-28.511 -27.586)">
+        <g
+          fill="none"
+          strokeDasharray="none"
+          strokeMiterlimit="4"
+          strokeOpacity="1"
+          transform="translate(31.781 -119.414)"
+        >
+          <rect
+            width="149.027"
+            height="150"
+            x="0"
+            y="147"
+            fillOpacity="1"
+            stroke="none"
+            strokeWidth="15.234"
+            ry="4.639"
+          ></rect>
+          <path
+            fillOpacity="0.474"
+            stroke="#8743cc"
+            strokeLinecap="round"
+            strokeWidth="31.879"
+            d="M108.177 264.699a57.024 57.396 0 01-79.75-2.6 57.024 57.396 0 01.905-80.306 57.024 57.396 0 0179.787-.78"
+          ></path>
+          <path
+            stroke="#8743cc"
+            strokeLinecap="round"
+            strokeLinejoin="miter"
+            strokeWidth="31"
+            d="M93.951 230.327h49.246v52.397"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default Logo;
